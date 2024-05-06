89330412b97aa9c56c1fc9db3e39919aa49624c3
ilakonovalov <antonpuckich@gmail.com>
2024-05-06 13:38:15 +0300

    Merge basic layout into root component and implement drawer toggling

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <v-app>
-    <v-main>
-      <navbar></navbar>
-      <drawer></drawer>
-      <v-main
-        class="d-flex align-center justify-center"
-        style="min-height: 300px"
-      >
-        Main Content
-      </v-main>
-    </v-main>
+    <v-app-bar>
+      <template v-slot:prepend>
+        <v-icon icon="$vuetify" />
+      </template>
+
+      <v-app-bar-title>User</v-app-bar-title>
+
+      <template v-slot:append>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </template>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" location="right">
+      <v-list>
+        <v-list-item link title="My Account"></v-list-item>
+        <v-list-item link title="Datasets"></v-list-item>
+        <v-list-item link title="Log out"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center"> Main Content </v-main>
   </v-app>
 </template>
 
 <script setup>
-//
+const drawer = defineModel()
+
 </script>
