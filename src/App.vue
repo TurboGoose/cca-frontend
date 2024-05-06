e335264e30e83db5006b5b313c82b878be05bb4e
ilakonovalov <antonpuckich@gmail.com>
2024-05-06 13:11:55 +0300

    Implement non-interactive drawer template

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,17 @@
   <v-app>
     <v-main>
       <navbar></navbar>
+      <drawer></drawer>
+      <v-main
+        class="d-flex align-center justify-center"
+        style="min-height: 300px"
+      >
+        Main Content
+      </v-main>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-  //
+//
 </script>
