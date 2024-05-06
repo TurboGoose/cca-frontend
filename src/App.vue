bf0f0d89a22f01727f6e5e7fb63acb013dd05684
ilakonovalov <antonpuckich@gmail.com>
2024-05-06 12:59:40 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <navbar></navbar>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar>
+    <template v-slot:prepend>
+        <v-icon icon="$vuetify" />
+    </template>
+
+    <v-app-bar-title>User</v-app-bar-title>
+
+    <template v-slot:append>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+  </v-app-bar>
+</template>
+
+<script>
+
+</script>
+
