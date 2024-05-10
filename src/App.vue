3034f31ed5b6233c456ea24ff2f0c5baa8241d28
ilakonovalov <antonpuckich@gmail.com>
2024-05-10 16:01:42 +0300

    Fix logo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <v-app>
     <v-app-bar>
-      <template v-slot:prepend>
-        <v-icon icon="$vuetify" />
-      </template>
-
-      <v-app-bar-title>User</v-app-bar-title>
+      <v-app-bar-title>CCA</v-app-bar-title>
 
       <template v-slot:append>
         <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
